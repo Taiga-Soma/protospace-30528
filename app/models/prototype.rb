f2245747478c0253cfef01589f58e74b9de0f42f
Taiga-Soma <allvictory111@gmail.com>
2020-10-14 12:30:27 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,12 @@
+class Prototype < ApplicationRecord
+  has_one_attached :image
+  has_many :comments
+  has_many :commnets, dependent: :destroy
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
+  belongs_to :user
+end
